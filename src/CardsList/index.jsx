88f1f0fbe0107266cb88009a2c782f4876977c7e
Nishant Kumar <nishant.29k1@gmail.com>
2025-02-24 03:11:53 +0530

    infinte-scroll-using-intersection-observer-browser-api

--- src/CardsList/index.jsx
+++ src/CardsList/index.jsx
@@ -1,53 +1,62 @@
-import { useEffect, useRef, useState } from 'react';
-import useCards from '../useCards';
+import { useEffect, useRef, useState } from "react";
+import useCards from "../useCards";
 
 const CardsList = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const cardList = useCards({ currentPage });
   const containerRef = useRef(null);
+  const lastCardRef = useRef(null);
 
   useEffect(() => {
-    const el = containerRef.current;
-    if (!el) return;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const lastCard = entries[0];
+        if (lastCard.isIntersecting) {
+          setCurrentPage((prevPage) => prevPage + 1);
+        }
+      },
+      { threshold: 1, root: containerRef.current }
+    );
 
-    const handleInfiniteScroll = () => {
-      const scrollAtBottom = el.scrollTop + el.clientHeight >= el.scrollHeight;
+    if (lastCardRef.current) {
+      observer.observe(lastCardRef.current);
+    }
 
-      if (scrollAtBottom) {
-        setCurrentPage((prev) => prev + 1);
+    return () => {
+      if (lastCardRef.current) {
+        observer.unobserve(lastCardRef.current);
       }
     };
-    el.addEventListener('scroll', handleInfiniteScroll);
-    return () => {
-      el.removeEventListener('scroll', handleInfiniteScroll);
-    };
-  }, []);
+  }, [cardList]);
 
   return (
     <div
       ref={containerRef}
       style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gap: '24px',
-        background: 'grey',
-        padding: '2rem',
-        overflowY: 'scroll',
-        height: '50vh',
-        width: '100vw',
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        width: "60vw",
+        margin: "20vh auto",
+        gap: "24px",
+        background: "grey",
+        textAlign: "center",
+        padding: "2rem",
+        overflowY: "scroll",
+        height: "50vh",
       }}
     >
       {Array.isArray(cardList) &&
-        cardList?.map((item) => {
+        cardList?.map((item, index) => {
           return (
             <div
+              ref={index === cardList.length - 1 ? lastCardRef : null}
               key={item.id}
               style={{
-                background: 'black',
-                height: '12rem',
-                width: '8rem',
-                color: 'white',
-                padding: '12px',
+                background: "black",
+                height: "12rem",
+                width: "8rem",
+                color: "white",
+                padding: "12px",
               }}
             >
               {item.title}
